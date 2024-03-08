dec448c02d1cdca2d949565416bdd5229dedf161
user <user@mail.ru>
2024-03-08 11:25:09 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	int choose;
+	do
+	{
+		cout << "1) Continue\n";
+		cout << "2) download\n";
+		cout << "3) Settings\n";
+		cout << "4) Exit\n";
+		cout << "5) create game\n";
+		cin >> choose;
+	} while (choose == 1);
+	{
+		cout << "Loanding";
+	}
+
+
+	return 0;
+}
